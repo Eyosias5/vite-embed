a3d47f8949d0d997812efbab17f8245d5d5adf0b
Eyosias5 <eyosiassamson5@gmail.com>
2023-02-02 16:26:54 +0300

    add react plugin

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [cssInjectedByJsPlugin()],
+  plugins: [react()],
   build:{
     cssCodeSplit: false,
     rollupOptions:{
