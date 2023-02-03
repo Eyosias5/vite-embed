deaeb37f4c1a27ab23713d4d8219ad8d003f7343
Eyosias5 <eyosiassamson5@gmail.com>
2023-02-03 16:49:58 +0300

    chores: clean

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,6 @@ import reactLogo from './assets/react.svg'
 import { createPortal } from 'react-dom';
 import './App.css'
 
-function generateDiscountCode() {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let discountCode = '';
-  for (let i = 0; i < 8; i++) {
-    discountCode += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return discountCode;
-}
-
 const discounts ={
   "SUMMER10": {type:"percentage", value:0.1},
  
